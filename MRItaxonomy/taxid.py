785f90c492cb0920e1dc23d7622d1deca6fcc9d7
Colin Price <colinprice@system76-pc.localdomain>
2024-02-21 15:37:11 -0500

    updated taxid.py to not reload nodes and merge each time

--- MRItaxonomy/taxid.py
+++ MRItaxonomy/taxid.py
@@ -5,32 +5,36 @@ import time
 import site
 import logging
 
+df = None
+df2 = None
 
 def load_dbs():
     logger = logging.getLogger(__name__)
-    
-    site_packages = site.getsitepackages()[0]
-    for root, dirs, files in os.walk(site_packages):
-        if "MRItaxonomy" in dirs:
-            nodedb_path = os.path.join(root, "MRItaxonomy")
-            break
+    global df
+    global df2
+    if df is None or df2 is None:
+        site_packages = site.getsitepackages()[0]
+        for root, dirs, files in os.walk(site_packages):
+            if "MRItaxonomy" in dirs:
+                nodedb_path = os.path.join(root, "MRItaxonomy")
+                break
+            else:
+                print("Bad installation location")
+                raise SystemExit
+        
+        nodedb_path=nodedb_path+"/dumps/"
+        if os.path.exists(nodedb_path+"nodes.dmp") and os.path.exists(nodedb_path+"merged.dmp"):
+            logger.warning("Nodes database was last modified: {}".format(time.ctime(os.path.getctime(nodedb_path+"nodes.dmp"))))
+            logger.warning("Merged database was last modified: {}".format(time.ctime(os.path.getctime(nodedb_path+"merged.dmp"))))
         else:
-            print("Bad installation location")
+            logger.critical("One or more database files are missing. Run update.py to download.")
             raise SystemExit
-    
-    nodedb_path=nodedb_path+"/dumps/"
-    if os.path.exists(nodedb_path+"nodes.dmp") and os.path.exists(nodedb_path+"merged.dmp"):
-        logger.warning("Nodes database was last modified: {}".format(time.ctime(os.path.getctime(nodedb_path+"nodes.dmp"))))
-        logger.warning("Merged database was last modified: {}".format(time.ctime(os.path.getctime(nodedb_path+"merged.dmp"))))
-    else:
-        logger.critical("One or more database files are missing. Run update.py to download.")
-        raise SystemExit
-    
-    headers = ("tax_id", "parent", "rank")
-    df = pd.read_table(nodedb_path+"nodes.dmp", sep="|", names=headers,index_col=0,usecols=(0,1,2))
-    df["rank"] = df["rank"].str.strip()
-    mergedheaders = ("original", "merged")
-    df2 = pd.read_table(nodedb_path+"merged.dmp", sep="|", names=mergedheaders,index_col=0,usecols=(0,1))
+        
+        headers = ("tax_id", "parent", "rank")
+        df = pd.read_table(nodedb_path+"nodes.dmp", sep="|", names=headers,index_col=0,usecols=(0,1,2))
+        df["rank"] = df["rank"].str.strip()
+        mergedheaders = ("original", "merged")
+        df2 = pd.read_table(nodedb_path+"merged.dmp", sep="|", names=mergedheaders,index_col=0,usecols=(0,1))
     return df, df2
 
 
