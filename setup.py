0242df70978bf9e70236f7318031ecda7a4add16
Colin Price <colinprice@system76-pc.localdomain>
2024-06-11 11:59:37 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MRItaxonomy",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=['pandas>=0.19.0',
                       'biopython>=1.7',
